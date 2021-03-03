06cfdbcf44183cb49b35f5d278c35af72830251f
Ifekhar Hossen <68419982+Iftekhar-Hossen@users.noreply.github.com>
2021-03-03 20:09:43 +0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -42,6 +42,64 @@ $(function () {
     slidesToScroll: 1,
     prevArrow: ".prevArrow",
     nextArrow: ".nextArrow",
+    responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
+
+  
+$('.center').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows:false,
+  autoplaySpeed: 2000,
+  centerMode:true,
+  centerPadding:false,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: '180px',
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1,
+        centerPadding: '100px',
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
   new WOW().init();
 });
+
