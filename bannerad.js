0317dcaaf12d0c36dcb0e01d4b2bac84878e7091
Ifekhar Hossen <68419982+Iftekhar-Hossen@users.noreply.github.com>
2021-06-06 14:26:03 +0600

    Create bannerad.js

---
+++ bannerad.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '046cdd17bf9ec4a4ae4ceedaa9a12d94',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://abortmaydesirable.com/046cdd17bf9ec4a4ae4ceedaa9a12d94/invoke.js"></scr' + 'ipt>');
+</script>
